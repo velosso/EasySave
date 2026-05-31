f4a51d29eddb3bab23411da18fa30423b6dc1b9a
velosso <franklindjoumessi576@gmail.com>
2026-05-31 15:23:43 +0100

    CONFLIT

--- EasySave/obj/Debug/net10.0/EasySave.AssemblyInfo.cs
+++ EasySave/obj/Debug/net10.0/EasySave.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EasySave")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85d314f6fa10854b7627177d9e1e76131fd02ac4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea3473f4eb2c19d1c9ddca4ec5fc99d9972ad381")]
 [assembly: System.Reflection.AssemblyProductAttribute("EasySave")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EasySave")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
